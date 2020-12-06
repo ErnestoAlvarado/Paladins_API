5775b38a7bb13e721306202c797fd3cad305061c
ErnestoAlvarado <ernesto.alvarado@ymail.com>
2020-12-06 00:18:00 -0800

    Restructured files

---
+++ champions/champion_roles.py
@@ -0,0 +1,12 @@
+
+
+CHAMPIONS = [('Bomb King', 'Damage'), ('Cassie', 'Damage'), ('Dredge', 'Damage'), ('Drogoz', 'Damage'), ('Imani', 'Damage'),
+             ('Kinessa', 'Damage'), ('Lian', 'Damage'), ('Sha Lin', 'Damage'), ('Strix', 'Damage'), ('Tiberius', 'Damage'),
+             ('Tyra', 'Damage'), ('Viktor', 'Damage'), ('Vivian', 'Damage'), ('Willo', 'Damage'), ('Ash', 'Front Line'),
+             ('Atlas', 'Front Line'), ('Barik', 'Front Line'), ('Fernando', 'Front Line'), ('Inara', 'Front Line'),
+             ('Khan', 'Front Line'), ('Makoa', 'Front Line'), ('Raum', 'Front Line'), ('Ruckus', 'Front Line'),
+             ('Terminus', 'Front Line'), ('Torvald', 'Front Line'), ('Androxus', 'Flank'), ('Buck', 'Flank'),
+             ('Evie', 'Flank'), ('Koga', 'Flank'), ('Lex', 'Flank'), ('Maeve', 'Flank'), ('Moji', 'Flank'),
+             ('Skye', 'Flank'), ('Talus', 'Flank'), ('Zhin', 'Flank'), ('Corvus', 'Support'), ('Furia', 'Support'),
+             ('Grohk', 'Support'), ('Grover', 'Support'), ('Io', 'Support'), ('Jenos', 'Support'), ("Mal'Damba", 'Support'),
+             ('Pip', 'Support'), ('Seris', 'Support'), ('Ying', 'Support')]
